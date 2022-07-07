ffa327d3033eafbf229bcc5351670af1a5dc7441
rugia <rugia813@yahoo.com.tw>
2022-07-07 17:26:31 +0800

    add PWA

---
+++ src/pwa/useServiceWorker.js
@@ -0,0 +1,31 @@
+import { useState, useCallback, useEffect } from "react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+export const useServiceWorker = () => {
+  const [waitingWorker, setWaitingWorker] = useState<ServiceWorker | null>(null);
+  const [showReload, setShowReload] = useState < boolean > false;
+  // called when a service worker
+  // updates. this function is a callback
+  // to the actual service worker
+  // registration onUpdate.
+  const onSWUpdate = useCallback((registration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+  }, []);
+  // simply put, this tells the service
+  // worker to skip the waiting phase and then reloads the page
+  const reloadPage = useCallback(() => {
+    waitingWorker?.postMessage({ type: "SKIP_WAITING" });
+    setShowReload(false);
+    window.location.reload();
+  }, [waitingWorker]);
+  // register the service worker
+  useEffect(() => {
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://cra.link/PWA
+    serviceWorkerRegistration.register({
+      onUpdate: onSWUpdate,
+    });
+  }, [onSWUpdate]);
+  return { showReload, waitingWorker, reloadPage };
+};
